fc2cd124f813c98e512b12c10f0464fb79142566
Jazmin Aguilar <Jazmin086441@outlook.es>
2025-05-24 19:21:59 -0600

    Se avanzo en la codificacion de el ejercio Multiplos de cinco

---
+++ 23052025/MultiplosDeCinco.java
@@ -0,0 +1,9 @@
+public class MultiplosDeCinco {
+    public static void main(String[] args) {
+        // Bucle que va desde 1 hasta 50
+        for (int i = 1; i <= 50; i++) {
+            int multiplo = i * 5; // Calcula el múltiplo de 5
+            System.out.println("Múltiplo " + i + " de 5: " + multiplo);
+        }
+    }
+}
